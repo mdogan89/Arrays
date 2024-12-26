579b57cfb82a92a5da7c4bc1ef99f55689b633c7
Mustafa Dogan <mdogan89@hotmail.com>
2024-12-26 15:13:03 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+
+Array.Sort(sayiDizisi);
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+
+Array.Clear(sayiDizisi,2,2);
+
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+
+
+Array.Reverse(sayiDizisi);
+
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
+
+Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+Array.Resize<int>(ref sayiDizisi, 9);
+
+sayiDizisi[8] = 99;
+
+foreach (var sayi in sayiDizisi)
+{
+    Console.WriteLine(sayi);
+}
